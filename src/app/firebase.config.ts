bf57e7921de5666198da5878e390f1c943a19144
Abhishek Raj <araj63386@gmail.com>
2026-05-07 10:38:42 +0530

    firebase

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth } from "firebase/auth";
-// Your web app's Firebase configuration
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDUCGcd2hjjuHqUqZllLC2PLPylUdjHxyE",
+  apiKey:"AIzaSyDUCGcd2hjjuHqUqZllLC2PLPylUdjHxyE",
   authDomain: "coverkart-f8d81.firebaseapp.com",
   projectId: "coverkart-f8d81",
   storageBucket: "coverkart-f8d81.firebasestorage.app",
@@ -13,6 +10,5 @@ const firebaseConfig = {
   appId: "1:233673387415:web:d68dbaf651e9e53a69e8d2"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
